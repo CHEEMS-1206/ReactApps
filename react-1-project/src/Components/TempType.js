bdf20c4bf4d1619b06aba9916db6a9f1026c00ef
PRIYANSHU SINGH <sentysingh456@gmail.com>
2022-12-31 13:07:43 +0530

    The block of the application that specifies the weather type of the area

---
+++ react-1-project/src/Components/TempType.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TempType.css";
+
+function TempType(props) {
+  return (
+    <div className="TempType">{props.TempType}
+    </div>
+  );
+}
+
+export default TempType;
