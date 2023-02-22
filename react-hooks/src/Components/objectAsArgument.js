f66e989b5cba43b2f754d6d2ca4e5917973a98fa
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-22 12:14:27 +0530

    objects as arguments to useState hook in react

---
+++ react-hooks/src/Components/objectAsArgument.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function ObjectAsArgument() {
+  const [state, setState] = useState({ count: 9, theme: "blue" }); // object as the argument
+  const count = state.count; // all states returned under a common name
+  const theme = state.theme;
+  function decrementCount() {
+    setState((prevState) => {
+      return { count: prevState.count - 1 }; // when changing a value the other parameters are not spread using spread operator
+    });
+  }
+  function incrementCount() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 }; // when changing a value the other parameters are spread using spread operator
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={incrementCount}>+</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={decrementCount} className="decColor">
+        -
+      </button>
+    </div>
+  );
+}
+
+export default ObjectAsArgument;
