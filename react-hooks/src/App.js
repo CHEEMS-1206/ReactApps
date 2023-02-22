df53d173987011e6d23e62b77b3a08e695dfee93
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-22 12:15:45 +0530

    app.js file for rendering the useState.js and argumentAsObject.js app.css for sttling and index.js for main render

---
+++ react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import UseState from "./Components/useState";
+import ObjectAsArgument from "./Components/objectAsArgument";
+
+function App() {
+  return (
+    <div className="app">
+      <UseState></UseState>
+      <ObjectAsArgument />
+    </div>
+  );
+}
+
+export default App;
