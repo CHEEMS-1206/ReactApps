ca8667b5bbd27e6d5e96cdfc11bd6f10485b43c4
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-22 13:18:55 +0530

    Re-adjusted some files across folders

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import React from "react";
-import UseState from "./Components/useState";
-import ObjectAsArgument from "./Components/objectAsArgument";
+import UseState from "./Components/useState/useState";
+import ObjectAsArgument from "./Components/useState/objectAsArgument";
+import UseEffect from "./Components/useEffect/useEffect"
 
 function App() {
   return (
     <div className="app">
       <UseState></UseState>
       <ObjectAsArgument />
+      <UseEffect/>
     </div>
   );
 }
